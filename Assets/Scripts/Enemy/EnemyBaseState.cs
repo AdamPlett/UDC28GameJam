a28549848ddd9b70264f5d12a8a668427a2b53d9
ItGage <Gdelosh18@gmail.com>
2023-12-08 13:01:12 -0600

    State Machine Basics

---
+++ Assets/Scripts/Enemy/EnemyBaseState.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyBaseState : State
+{
+    protected readonly EnemyStateMachine stateMachine;
+
+    protected EnemyBaseState(EnemyStateMachine stateMachine)
+    {
+
+    }
+
+    protected void TravelToNextPoint()
+    {
+
+    }
+
+    protected void TravelToSound()
+    {
+
+    }
+
+    protected void Attack()
+    {
+
+    }
+}

1be4ab818984e297012dd5d8393238013d82a7f5
ItGage <Gdelosh18@gmail.com>
2023-12-08 17:14:56 -0600

    Added Enemy Basics
    Added a basic enemy state machine, with only the patrol state currently implemented. Also added an animation folder with several enemy animations.

--- Assets/Scripts/Enemy/EnemyBaseState.cs
+++ Assets/Scripts/Enemy/EnemyBaseState.cs
@@ -1,28 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using static GameManager;
+
+public enum eState { None, Patrolling, Searching, Chasing, Attacking, Wandering }
 
 public abstract class EnemyBaseState : State
 {
     protected readonly EnemyStateMachine stateMachine;
 
+    private int patrolIndex = 0;
+
+    private Transform targetLocation;
+    private NavMeshAgent navAgent;
+    private GameObject player;
+
     protected EnemyBaseState(EnemyStateMachine stateMachine)
     {
+        this.stateMachine = stateMachine;
 
+        navAgent = stateMachine.navAgent;
+        player = gm.playerInstance;
+
+        TravelToNextPoint();
+    }
+
+    protected void CheckTargetDistance()
+    {
+        if(stateMachine.activeState == eState.Patrolling)
+        {
+            if (navAgent.remainingDistance < 1f)
+            {
+                TravelToNextPoint();
+            }
+        }
     }
 
     protected void TravelToNextPoint()
     {
-
+        patrolIndex = (patrolIndex + 1) % stateMachine.patrolPoints.Length;
+        navAgent.SetDestination(stateMachine.patrolPoints[patrolIndex].position);
     }
 
-    protected void TravelToSound()
+    protected void TravelToSound(Transform soundTransform)
     {
-
+        navAgent.SetDestination(soundTransform.position);
     }
 
     protected void Attack()
     {
-
+        // Kill the fucking player
+        // Make them shit their pants
     }
 }
--- Assets/Scripts/Enemy/EnemyStateMachine.cs
+++ Assets/Scripts/Enemy/EnemyStateMachine.cs
@@ -1,11 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class EnemyStateMachine : MonoBehaviour
+public class EnemyStateMachine : StateMachine
 {
+    public eState activeState;
+
+    [Header("Movement")]
+    public float moveSpeed;
+    public float chaseSpeed;
+
+    [Header("Detection")]
+    public float detection;
+    public float hearRadius;
+    public float seeRadius;
+    public Transform lastHeard;
+
+    [Header("Navigation")]
+    public NavMeshAgent navAgent;
+    public Transform[] patrolPoints;
+
     private void Start()
     {
-        
+        SwitchState(new EnemyPatrolState(this));
     }
 }
